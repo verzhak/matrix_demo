7c6f1034dec95b23ef8ed1bd8ce29019f2e2640b
verzhak <verzhak@gmail.com>
2009-08-01 19:41:14 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#coding=utf-8
+
+#############################################
+#   Author: Maxim "verzhak" Akinin          #
+#   E-mail: verzhak@gmail.com               #
+#############################################
+#   Version: TODO                           #
+#   Date: 01.08.2009                        #
+#   License: GPLv3                          #
+#############################################
+
+
